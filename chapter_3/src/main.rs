2dcb7bea7c09ef52e3cc057c7d8d7aa04dd91749
JohnMckane <jmckane07@qub.ac.uk>
2025-10-18 14:35:26 +0100

    use shadowing

--- chapter_3/src/main.rs
+++ chapter_3/src/main.rs
@@ -4,4 +4,18 @@ fn main() {
     println!("x is {x}");
     x = 6;
     println!("x is {x}");
+    println!("Shadow example");
+    let x = 9;
+    println!("x is {x}");
+    {
+        let x = 6;
+    println!("x is {x} in this scope");
+    }
+    println!("x is {x} in main");
+    println!("Scope to change type");
+    let spaces = "     ";
+    println!("spaces: {spaces}");
+    let spaces = spaces.len();
+    println!("spaces: {spaces}");
+
 }
