a7eae72174e15031a5ea5ad24f91ecb5d47efc5c
JohnMckane <jmckane07@qub.ac.uk>
2025-10-18 18:32:25 +0100

    Errors handled

--- error_hangling_and_arrays/src/main.rs
+++ error_hangling_and_arrays/src/main.rs
@@ -1,19 +1,30 @@
 use std::io;
 fn main() {
-        let a = [1, 2, 3, 4 ,5];
-        let b = [2; 5];
-        loop {
-            println!("Enter index");
-            let mut index = String::new();
-            io::stdin()
-                .read_line(&mut index)
-                .expect("Failed to read line");
-            let index: usize = index
+    let a = [1, 2, 3, 4 ,5];
+    let b = [2; 5];
+    loop {
+        println!("Enter index");
+        let mut index = String::new();
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to read line");
+        let index = match index
             .trim()
-            .parse()
-            .expect("Index entered was not a number");
+            .parse() {
+                Ok(index) => (index, true),
+                Err(_error) => (0, false),
+            };
+            if !index.1 {
+                println!("Did not parse int");
+                continue;
+            }
+            let index = index.0;
+            if index < a.len() && index < a.len(){
             let b_element = b[index];
             let a_element = a[index];
             println!("a[{}]: {},b[{}]: {}", index, index, a_element, b_element);
+            }else {
+                println!("out of bounds");
         }
+    }
 }
