224cd55cf94fd5ee9c6a07774a4ed7684ec6e2f0
JohnMckane <jmckane07@qub.ac.uk>
2025-10-31 21:36:53 +0000

    Add error handling

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -1,11 +1,16 @@
 use std::fs;
 use std::env;
+use std::error::Error;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap();
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 struct Config {
     query: String,
@@ -19,9 +24,8 @@ impl Config {
         Ok(Config {query: args[1].clone(), file_path: args[2].clone()})
     }
 }
-fn run(config: Config){
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+    Ok(())
 }
