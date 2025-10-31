e848e2135b0e27e97b0c3097e8b4224803c419a1
JohnMckane <jmckane07@qub.ac.uk>
2025-10-31 21:20:01 +0000

    extract run

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -2,21 +2,26 @@ use std::fs;
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap();
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    run(config);
 }
 struct Config {
     query: String,
     file_path: String
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
-        Config {query: args[1].clone(), file_path: args[2].clone()}
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough args")
+        }
+        Ok(Config {query: args[1].clone(), file_path: args[2].clone()})
     }
+}
+fn run(config: Config){
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
