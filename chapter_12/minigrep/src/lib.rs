d36036b53442703e29d397f76a1580d717d7f162
JohnMckane <jmckane07@qub.ac.uk>
2025-11-08 21:47:28 +0000

    tests now pass

--- chapter_12/minigrep/src/lib.rs
+++ chapter_12/minigrep/src/lib.rs
@@ -1,5 +1,11 @@
-pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
-    vec!["safe, fast, productive."]
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 #[cfg(test)]
 mod tests {
